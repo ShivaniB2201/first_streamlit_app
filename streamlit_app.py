179b83de8e97e003005f824cc83fde4763972db2
Shivani Bhardwaj <112691283+ShivaniB2201@users.noreply.github.com>
2022-09-09 13:15:58 +0530

    Update streamlit_app.py
    Importing Data

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,6 @@ streamlit.header('😊Qualities of good husband')
 streamlit.text('😍Loving')
 streamlit.text('🥰Caring')
 streamlit.header('How to build your own smoothies')
+import pandas
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
